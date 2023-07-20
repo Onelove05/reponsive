9ae52921641e93d4d94d6cd31dd8abe62e1a2aac
Onelove05 <D.Onelove05@gmail.com>
2023-07-20 20:41:11 +0700

    reponsive

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(function(){
     $('.slick').slick({
       Infinity:true,
@@ -42,6 +45,13 @@ $(document).ready(function(){
           },
           {
             breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+            }
+          },
+          {
+            breakpoint: 576,
             settings: {
               slidesToShow: 2,
               slidesToScroll: 1,
@@ -104,3 +114,69 @@ $(document).ready(function(){
         ]
     });
   });
+
+  $('.slick-image-big').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slick-image-small'
+  });
+  $('.slick-image-small').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slick-image-big',
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          }
+        },
+        // {
+        //   breakpoint: 767,
+        //   settings: {
+        //     slidesToShow: 3,
+        //     slidesToScroll: 1,
+        //   }
+        // },
+        // {
+        //   breakpoint: 480,
+        //   settings: {
+        //     slidesToShow: 1,
+        //     slidesToScroll: 1
+        //   }
+        // }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
+
+
+  $('input.input-qty').each(function() {
+    var $this = $(this),
+      qty = $this.parent().find('.is-form'),
+      min = Number($this.attr('min')),
+      max = Number($this.attr('max'))
+    if (min == 0) {
+      var d = 0
+    } else d = min
+    $(qty).on('click', function() {
+      if ($(this).hasClass('minus')) {
+        if (d > min) d += -1
+      } else if ($(this).hasClass('plus')) {
+        var x = Number($this.val()) + 1
+        if (x <= max) d += 1
+      }
+      $this.attr('value', d).val(d)
+    })
+  })
